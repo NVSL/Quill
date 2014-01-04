d9aa06c4d5fbeda8c0065a8ee108c829dd91e8e5
Andiry Xu <jix024@cs.ucsd.edu>
2014-01-03 16:45:02 -0800

    Add atomic operations to replace pthread locks, saves ~20ns for each lock.

--- nvp_lock.h
+++ nvp_lock.h
@@ -23,9 +23,14 @@ static inline int _nvp_get_cpuid(void)
 	return id;
 }
 
+#ifndef USE_PTHREAD_LOCK
+	#define	USE_PTHREAD_LOCK 1
+#endif
 
 // double the number of logical cores: each lock takes up half a cache line, so to
 // reduce contention we space them out across cache lines.
+#if	USE_PTHREAD_LOCK
+
 #define NVP_NUM_LOCKS 32
 
 #define NVP_LOCK_DECL pthread_rwlock_t lock[NVP_NUM_LOCKS]
@@ -83,3 +88,54 @@ static inline int _nvp_get_cpuid(void)
 	//	DEBUG("NVP_WR Locking %i\n", iter);
 	//	DEBUG("NVP_WR Unlocking %i\n", iter);
 
+#else
+
+#define NVP_NUM_LOCKS 32
+
+#define WR_HELD	(1 << 31)
+
+#define NVP_LOCK_DECL uint32_t lock[NVP_NUM_LOCKS]
+
+#define NVP_LOCK_INIT(lock) { int iter; for(iter=0; iter<NVP_NUM_LOCKS; iter++) { lock[iter] = 0; } }
+
+	#define NVP_LOCK_CHECK(statement) statement
+
+#define SANITY assert
+
+#define NVP_LOCK_RD(lock, cpuid) \
+	cpuid = _nvp_get_cpuid(); \
+	SANITY(cpuid<(NVP_NUM_LOCKS/2)); \
+	DEBUG("NVP_RDLOCK requested on CPU %i\n", cpuid); \
+	while(__sync_fetch_and_add(&lock[cpuid * 2], 1) >= WR_HELD) \
+		__sync_fetch_and_sub(&lock[cpuid * 2], 1); \
+	DEBUG("NVP_RDLOCK acquired on CPU %i\n", cpuid)
+
+#define NVP_LOCK_UNLOCK_RD(lock, cpuid) \
+	DEBUG("Releasing NVP_RDLOCK on CPU %i\n", cpuid); \
+	SANITY(cpuid<(NVP_NUM_LOCKS/2)); \
+	__sync_fetch_and_sub(&lock[cpuid * 2], 1); \
+	DEBUG("NVP_RDLOCK released on CPU %i\n", cpuid)
+
+
+#define NVP_LOCK_WR(lock) { int iter; \
+	DEBUG("NVP_WRLOCK requested on cpu %i\n", _nvp_get_cpuid()); \
+	for(iter=0; iter<NVP_NUM_LOCKS; iter+=2) { \
+		while(!__sync_bool_compare_and_swap(&lock[iter], 0, WR_HELD)) \
+			; \
+	} \
+	DEBUG("NVP_WRLOCK acquired on cpu %i\n", _nvp_get_cpuid()); \
+	}
+
+#define NVP_LOCK_UNLOCK_WR(lock) { int iter; \
+	DEBUG("NVP_WRLOCK releasing on cpu %i\n", _nvp_get_cpuid()); \
+	for(iter=0; iter<NVP_NUM_LOCKS; iter+=2) { \
+		__sync_fetch_and_sub(&lock[iter], WR_HELD); \
+	} \
+	DEBUG("NVP_WRLOCK released on cpu %i\n", _nvp_get_cpuid()); \
+	}
+
+
+	//	DEBUG("NVP_WR Locking %i\n", iter);
+	//	DEBUG("NVP_WR Unlocking %i\n", iter);
+
+#endif
